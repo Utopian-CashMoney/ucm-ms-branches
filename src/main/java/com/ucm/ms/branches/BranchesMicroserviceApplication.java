0eccd00c47a29a588470d178b6173413fd7fb0f7
Josten Asercion <josten.asercion@smoothstack.com>
2021-05-17 10:44:20 -0400

    Initial Commit

---
+++ src/main/java/com/ucm/ms/branches/BranchesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ucm.ms.branches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucm/ms/branches/BranchesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucm.ms.branches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
