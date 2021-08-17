8efc38770715074a576e4f7cce915bf383086414
Josten Asercion <josten.asercion@smoothstack.com>
2021-08-16 23:50:51 -0400

    Added temporary H2 data for type and phone

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 --Branches
-INSERT INTO `branch` VALUES ( 1, 'East Branch', '22315', '1337 Sleepdeprived Road', 'Alexandria', '13:00:00', '23:00:00' );
-INSERT INTO `branch` VALUES ( 2, 'West Branch', '22079', '1234 ICouldGoForTacoBellRn Boulevard', 'Lorton', '08:00:00', '21:00:00' );
-INSERT INTO `branch` VALUES ( 3, 'South Branch', '23320', '420 Hardstuck Lane', 'Chesapeake', '7:00:00', '18:00:00' );
+INSERT INTO `branch` VALUES ( 1, 'East Branch', '22315', '1337 Sleepdeprived Road', 'Alexandria', '13:00:00', '23:00:00', 'ATM', '7031234567');
+INSERT INTO `branch` VALUES ( 2, 'West Branch', '22079', '1234 ICouldGoForTacoBellRn Boulevard', 'Lorton', '08:00:00', '21:00:00', 'FULL SERVICE', '5711234567');
+INSERT INTO `branch` VALUES ( 3, 'South Branch', '23320', '420 Hardstuck Lane', 'Chesapeake', '7:00:00', '18:00:00', 'FULL SERVICE', '7571234567');
 
 --Bankers
 INSERT INTO `banker` VALUES ( 1, 1, 'Soul Reaver Draven' );
